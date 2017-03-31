101eb03aa9ca9563b8eeb7c5914f0aea229c7148
Kamil Wrobel <xwrobelekx@Ewas-MBP.home>
2017-03-31 15:47:26 -0400

    we can create users, log in, and log out

---
+++ snap1/snap1/SnapsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SnapsViewController.swift
+//  snap1
+//
+//  Created by Kamil Wrobel on 3/31/17.
+//  Copyright © 2017 Kamil Wrobel. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    @IBAction func logoutTapped3(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        print("logged Out")
+        
+    }
+   
+
+
+
+}
