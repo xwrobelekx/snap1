73e7d5a7cd2db9a5f87927975d2f0f29b37817cd
Kamil Wrobel <xwrobelekx@Ewas-MBP.home>
2017-03-31 12:39:01 -0400

    added cocoPads, firebase

---
+++ snap1/Pods/Target Support Files/Pods-snap1/Pods-snap1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_snap1 : NSObject
+@end
+@implementation PodsDummy_Pods_snap1
+@end
---
+++ snap1/snap1/SignInViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SignInViewController.swift
+//  snap1
+//
+//  Created by Kamil Wrobel on 3/30/17.
+//  Copyright © 2017 Kamil Wrobel. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+  
+    @IBAction func turnUpTapped(_ sender: Any) {
+        
+    FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+        print("we tried to sign in@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        if error != nil {
+            print("We have an error: \(error)@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            
+            FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                print("we tried to create user ??????????????????????????????????????????????????????????????????????????????????????????????????")
+                if error != nil {
+                    print("we have an error: \(error)")
+                } else {
+                    print("created user sucesfullyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
+                }
+            })
+            
+            
+            
+            
+        } else {
+            print("Signed in Succesfully!################################################################################################################################################################")
+        }
+    })
+        
+    }
+
+
+}
+
