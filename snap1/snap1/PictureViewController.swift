ae5f8c8699e1ced0ea06e1a7c1711b9009e241a1
Kamil Wrobel <xwrobelekx@Ewas-MBP.home>
2017-04-01 22:45:08 -0400

    we can now select an image, the camera is all set up, but instead of using camera were picking an image from photo library

--- snap1/snap1/PictureViewController.swift
+++ snap1/snap1/PictureViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PictureViewController: UIViewController {
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -16,6 +16,7 @@ class PictureViewController: UIViewController {
     
     @IBOutlet weak var nextButtonOutlet: UIButton!
     
+    var imagePicker = UIImagePickerController()
     
     
     
@@ -23,10 +24,37 @@ class PictureViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
+        
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        
+        imageView.backgroundColor = UIColor.clear
+        
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    
 
     @IBAction func cameraTapped(_ sender: Any) {
         
+        imagePicker.sourceType = .savedPhotosAlbum
+        
+        imagePicker.allowsEditing = false
+        
+        
+        present(imagePicker, animated: true, completion: nil)
         
         
     }
@@ -34,7 +62,7 @@ class PictureViewController: UIViewController {
     
     @IBAction func nextButtonTapped(_ sender: Any) {
         
-        
+        performSegue(withIdentifier: selectUserSegue, sender: nil)
         
         
     }
